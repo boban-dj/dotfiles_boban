c70f550298b5a78bba0157fb5cedc15e3bafd1ce
bobandj71 <bobandj71@gmail.com>
2016-05-16 17:42:05 +0200

    dropped

--- BK/Scripts/jpg2multipdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-## Concatenate all .jpg to 1 multipage pdf in a4 format
-## Some quality loss
-
-convert *.jpg -compress jpeg -resize 1240x1753 -extent 1240x1753 -gravity center -units PixelsPerInch -density 150x150 multipage.pdf
